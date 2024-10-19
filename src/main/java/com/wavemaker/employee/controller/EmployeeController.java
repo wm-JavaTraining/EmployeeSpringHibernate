cb942aa6697a01f371aaa7245c2598df17bf46c5
RoopaAenugu <roopaaenugu2002@gmail.com>
2024-10-19 20:26:28 +0530

    implement employee crud operations using spring mvc

--- src/main/java/com/wavemaker/employee/controller/EmployeeController.java
+++ src/main/java/com/wavemaker/employee/controller/EmployeeController.java
@@ -3,10 +3,9 @@ package com.wavemaker.employee.controller;
 import com.wavemaker.employee.pojo.Employee;
 import com.wavemaker.employee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/employeeManagement")
@@ -17,8 +16,28 @@ public class EmployeeController {
 
 
     @PostMapping
-    public int  addEMployee(@RequestBody Employee employee) {
+    public int addEMployee(@RequestBody Employee employee) {
         return employeeService.addEmployee(employee);
+    }
+
+    @GetMapping("/{empId}")  // Using path variable
+    public Employee getEmployeeByEmpId(@PathVariable int empId) {
+        return employeeService.getEmployeeById(empId);
+    }
+
+    @GetMapping("/getAllEmployees")
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @DeleteMapping("/{empId}")
+    public boolean deleteEmployee(@PathVariable int empId) {
+        return employeeService.deleteEmployee(empId);
+    }
+
+    @PutMapping
+    public boolean updateEmployee(@RequestBody Employee employee) {
+        return employeeService.updateEmployee(employee);
     }
 
 }
